954befda0449650ab21314d3ff97db4cee9c9cc8
Fahim Shariar <107372111+fahimshariar28@users.noreply.github.com>
2023-06-07 17:56:26 +0600

    Update Classes

--- src/Pages/Classes/Classes/Classes.jsx
+++ src/Pages/Classes/Classes/Classes.jsx
@@ -1,9 +1,43 @@
-const Classes = () => {
-  return (
-    <div>
-      <h2 className="text-3xl text-center">This is Classes Page</h2>
-    </div>
-  );
-};
-
-export default Classes;
+import { useQuery } from "@tanstack/react-query";  
+  
+ const Classes = () => { 
+   const { data } = useQuery({ 
+     queryKey: ["classes"], 
+     queryFn: async () => { 
+       const res = await fetch("http://localhost:5000/classes"); 
+       return res.json(); 
+     }, 
+   }); 
+   console.log(data); 
+   return ( 
+     <div className="mb-3"> 
+       <h2 className="text-3xl text-center">Popular Classes</h2> 
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3"> 
+         {data?.map((item) => ( 
+           <> 
+             <div className="card w-96 bg-base-100 shadow-xl"> 
+               <figure> 
+                 <img src={item.image} alt="" /> 
+               </figure> 
+               <div className="card-body"> 
+                 <h2 className="card-title">{item.name}</h2> 
+                 <p className="text-2xl font-semibold"> 
+                   Instructor: {item.instructor} 
+                 </p> 
+                 <div className="flex justify-evenly"> 
+                   <p>Total Enrolled: {item.enrolled}</p> 
+                   <p>Price: {item.price}</p> 
+                 </div> 
+                 <div className="card-actions justify-end"> 
+                   <button className="btn btn-primary">Buy Now</button> 
+                 </div> 
+               </div> 
+             </div> 
+           </> 
+         ))} 
+       </div> 
+     </div> 
+   ); 
+ }; 
+  
+ export default Classes;
