37464ac20e3f47be36c94262b035b61a66785e6c
Fahim Shariar <fahimshariar28@gmail.com>
2023-06-07 16:43:16 +0600

    POpular classes section added to home page

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/MainRoute";
 import AuthProvider from "./provider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <div className="max-w-screen-xl mx-auto">
-        <RouterProvider router={router}></RouterProvider>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router}></RouterProvider>
+        </div>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
 );
