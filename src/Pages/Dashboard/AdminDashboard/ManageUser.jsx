aadbc64e173d9603b88a3691814a335cd22bb86e
Fahim Shariar <fahimshariar28@gmail.com>
2023-06-10 17:24:42 +0600

    all user shown and role change

--- src/Pages/Dashboard/AdminDashboard/ManageUser.jsx
+++ src/Pages/Dashboard/AdminDashboard/ManageUser.jsx
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useAuth from "../../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const ManageUser = () => {
   const { user, loading } = useAuth();
   const [axiosSecure] = useAxiosSecure();
 
-  const { data } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: ["users"],
     enabled:
       !loading && !!user?.email && !!localStorage.getItem("access-token"),
@@ -15,25 +16,70 @@ const ManageUser = () => {
       return res.data;
     },
   });
+
+  const handleMakeAdmin = async (user) => {
+    const res = await axiosSecure.put(`/makeAdmin/${user._id}`);
+    if (res.data.modifiedCount > 0) {
+      refetch();
+      Swal.fire({
+        icon: "success",
+        title: ` ${user.name} is now an admin`,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
+  const handleMakeInstructor = async (user) => {
+    const res = await axiosSecure.put(`/makeInstructor/${user._id}`);
+    if (res.data.modifiedCount > 0) {
+      refetch();
+      Swal.fire({
+        icon: "success",
+        title: ` ${user.name} is now an instructor`,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
   return (
     <div>
-      <h2>Manage user</h2>
+      <h2 className="text-2xl uppercase my-3 text-center">Manage user</h2>
       <table className="table w-full">
         <thead>
           <tr>
-            <th>Id</th>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
+            <th></th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {data?.map((user) => (
             <tr key={user._id}>
-              <td>{user._id}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>{user.role}</td>
+              <th>
+                <button
+                  onClick={() => handleMakeAdmin(user)}
+                  className="btn btn-primary btn-sm"
+                  disabled={user.role === "admin"}
+                >
+                  Make Admin
+                </button>
+              </th>
+              <th>
+                <button
+                  onClick={() => handleMakeInstructor(user)}
+                  className="btn btn-warning btn-sm"
+                  disabled={user.role === "instructor"}
+                >
+                  Make Instructor
+                </button>
+              </th>
             </tr>
           ))}
         </tbody>
