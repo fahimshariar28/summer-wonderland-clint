09c8045317cd294b1289a5352a1e77200177352f
Fahim Shariar <fahimshariar28@gmail.com>
2023-06-11 03:05:23 +0600

    my classes data fetch

--- src/Pages/Dashboard/InstructorDashboard.jsx/MyClasses.jsx
+++ src/Pages/Dashboard/InstructorDashboard.jsx/MyClasses.jsx
@@ -1,7 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
+
 const MyClasses = () => {
+  const { user, loading } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
+  const { data: classes, isLoading } = useQuery({
+    queryKey: ["classes"],
+    enabled: !loading && !!user,
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/instructorClasses?email=${user.email}`
+      );
+      return res.data;
+    },
+  });
+  console.log(classes);
+  if (isLoading) {
+    <div className="flex justify-center items-center">
+      <span className="loading loading-bars loading-lg"></span>
+    </div>;
+  }
+
   return (
     <div>
       <h2 className="text-2xl uppercase text-center">My Classes</h2>
+      {classes?.map((classItem) => (
+        <div key={classItem._id}></div>
+      ))}
     </div>
   );
 };
