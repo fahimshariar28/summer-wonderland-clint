d21cb563429cac613458a5b23089c4dcac837222
Fahim Shariar <fahimshariar28@gmail.com>
2023-06-10 22:58:30 +0600

    Banner change

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,19 +1,29 @@
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
 const Banner = () => {
   return (
-    <Carousel className="text-center">
-      <div>
-        <img src="https://i.ibb.co/YQy3GXX/images.jpg" />
+    <>
+      <div className="carousel w-full">
+        <div id="item1" className="carousel-item w-full">
+          <img src="https://i.ibb.co/YQy3GXX/images.jpg" className="w-full" />
+        </div>
+        <div id="item2" className="carousel-item w-full">
+          <img src="https://i.ibb.co/XtP69p8/images-1.jpg" className="w-full" />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img src="https://i.ibb.co/TqZNWnT/images-2.jpg" className="w-full" />
+        </div>
       </div>
-      <div>
-        <img src="https://i.ibb.co/XtP69p8/images-1.jpg" />
+      <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" className="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs">
+          3
+        </a>
       </div>
-      <div>
-        <img src="https://i.ibb.co/TqZNWnT/images-2.jpg" />
-      </div>
-    </Carousel>
+    </>
   );
 };
 
